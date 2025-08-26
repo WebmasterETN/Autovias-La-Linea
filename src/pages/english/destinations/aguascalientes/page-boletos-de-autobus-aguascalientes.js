5204c7f38e7b6ee59141556899545a28ba4a1fb9
JhonDev <153306997+JhonEG-17@users.noreply.github.com>
2025-08-26 16:22:39 -0600

    fix: imports

--- src/pages/english/destinations/aguascalientes/page-boletos-de-autobus-aguascalientes.js
+++ src/pages/english/destinations/aguascalientes/page-boletos-de-autobus-aguascalientes.js
@@ -27,9 +27,9 @@ class PageBoletosAutobusAguascalientesEnglish extends HTMLElement {
         <app-description-destination
           title-text="Aguascalientes is waiting. ETN will take you there."
           image-data='[
-            {"id": "1", "src": "../../src/assets/img/english/destinations/aguascalientes-english/templo-san-antonio-paduba.webp", "alt": "San Marcos Fair"},
-            {"id": "2", "src": "../../src/assets/img/english/destinations/aguascalientes-english/museo-aguascalientes.webp", "alt": "Cathedral Basilica of Our Lady of the Assumption"},
-            {"id": "3", "src": "../../src/assets/img/english/destinations/aguascalientes-english/templo-san-antonio.webp", "alt": "Tres Centurias Park"}
+            {"id": "1", "src": "../../src/assets/img/english/destinations/aguascalientes-english/overflow-images/templo-san-antonio-paduba.webp", "alt": "San Marcos Fair"},
+            {"id": "2", "src": "../../src/assets/img/english/destinations/aguascalientes-english/overflow-images/museo-aguascalientes.webp", "alt": "Cathedral Basilica of Our Lady of the Assumption"},
+            {"id": "3", "src": "../../src/assets/img/english/destinations/aguascalientes-english/overflow-images/templo-san-antonio.webp", "alt": "Tres Centurias Park"}
           ]'
           paragraph-text="Aguascalientes is a hidden gem in the heart of Mexico. Renowned for its warm hospitality, colonial architecture, vibrant festivals, and one of a kind museums, this destination blends history, culture, and modern charm in every corner. From the iconic San Marcos Fair to its temples, plazas, and cultural spaces, Aguascalientes offers an authentic and safe experience for travelers seeking to explore the soul of Mexico — with style and peace of mind."
         ></app-description-destination>
